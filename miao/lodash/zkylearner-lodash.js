cca07bdbb0c89e6e04dd381843e42ba68fa8d7ac
zhaokangyu <503545429@qq.com>
2019-07-10 19:50:56 +0800

    add lodash-chunk

--- miao/lodash/zkylearner-lodash.js
+++ miao/lodash/zkylearner-lodash.js
@@ -1,4 +1,18 @@
 var zkylearner = {
+    chunk: function (array, size = 1) {
+        if(size === 1){return array.slice()}
+        var result = []
+        var block = []
+        for(let i in array) {
+            block.push(array[i])
+            if((+i + 1) % size === 0) {
+                result.push(block)
+                block = []
+            }
+        }
+        if(block.length !== 0) {result.push(block)}
+        return result
+    },
     compact: function (ary) {
         return ary.filter(it => it)
     },
